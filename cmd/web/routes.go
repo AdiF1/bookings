42b386330aa119eeb4db2a1cfff8f0ac64b0aba8
Adi Fletcher <adifletcher@mac.com>
2021-09-19 19:57:44 -0700

    Added image folder

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/AdiF1/solidity/bookings/pkg/config"
@@ -22,6 +23,26 @@ func routes (app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// FileServer returns a handler that serves HTTP requests with the contents 
+	// of the file system rooted at root.
+
+	// As a special case, the returned file server redirects any request ending 
+	// in "/index.html" to the same path, without the final "index.html".
+
+	// To use the operating system's file system implementation, use http.Dir:
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	//Handle adds the route `pattern` that matches any http method to execute the `handler` http.Handler.
+
+	// StripPrefix returns a handler that serves HTTP requests by removing the given prefix from the 
+	// request URL's Path (and RawPath if set) and invoking the handler h. StripPrefix handles a request 
+	// for a path that doesn't begin with prefix by replying with an HTTP 404 not found error. The prefix 
+	// must match exactly: if the prefix in the request contains escaped characters the reply is also an 
+	// HTTP 404 not found error.
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+	fmt.Println(fileServer)
+
 
 	return mux
 }
