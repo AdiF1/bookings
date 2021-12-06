2f866e38db4cb6da773faffd286ad102f05b0ef3
Adi Fletcher <adifletcher@mac.com>
2021-12-05 18:28:20 -0800

    changed db user

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 go build -o bookings cmd/web/*.go
-./bookings -db_name=bookings2 -db_user=ms -cache=false -production=false
+./bookings -db_name=bookings2 -db_user=postgres -cache=false -production=false
